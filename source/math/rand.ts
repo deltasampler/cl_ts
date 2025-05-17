2ef717f805f03e212518bb88c3004b485f3b5050
deltasampler <deltasampler@gmail.com>
2025-05-17 11:55:19 +0300

    add frand

--- source/math/rand.ts
+++ source/math/rand.ts
@@ -7,3 +7,7 @@ export function rand_ex(min: number, max: number): number {
 export function rand_in(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export function frand_in(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+}
